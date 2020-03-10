a7909ddcd56f39e75ffed5f448fff749849f0ea3
alex19861108@live.cn <liuw@firstshare.cn>
2020-03-10 12:38:58 +0800

    add code

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 WORKPATH=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
+HOMEPATH=$(dirname $WORKPATH)
+LOGPATH=$HOMEPATH/log
+
+[ ! -d $LOGPATH ] && mkdir -p $LOGPATH
+
 HOST="0.0.0.0"
 PORT="8000"
-nohup venv/bin/uliweb runserver -h ${HOST} -p ${PORT} 1>log.stdout 2>&1 &
+
+mode=$@
+if [ "X$mode" == "Xrunserver" ]; then
+	docker run -p $PORT:$PORT djsearch:0.1
+else
+	nohup venv/bin/uliweb runserver -h ${HOST} -p ${PORT} 1>$LOGPATH/log.stdout 2>&1 &
